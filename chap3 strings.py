19f44a2402f5d816bcbd3dca72522dde4f8b0801
5hivam45 <shivam2722007@gmail.com>
2026-09-11 22:46:08 +0530

    notes

--- chap3 strings.py
+++ chap3 strings.py
@@ -1,8 +1,23 @@
 # STRING:
 # STRING IS A DATA TYPE IN PYTHON :
 
-a='shivam' #single quoted string 
-b="shivam" #double quoted string 
-c='''shivam''' #triple quoted string 
+# a='shivam' #single quoted string 
+# b="shivam" #double quoted string 
+# c='''shivam''' #triple quoted string
+
+# STRING SLICING:
+# indexing
+# name = "shivamkumar"
+# nameshort=name[0:3] # start form index 0 all the way till 3 (excluding 3)
+# print(nameshort)
 
 
+# name="shivang"
+# character1=name[1]
+# print(character1)
+
+# sl=name[ind_start:ind_end]
+  #first index include    #last index is not icluded
+  
+# sl[0:3]returns "shi"---characters from 0to3
+# sl[1:3]returns "hi"---characters from 1to3

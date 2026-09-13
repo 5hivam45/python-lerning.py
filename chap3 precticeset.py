58ba5f69d041638731b5d617190577205db68984
5hivam45 <shivam2722007@gmail.com>
2026-09-13 22:53:43 +0530

    notes

--- chap3 precticeset.py
+++ chap3 precticeset.py
@@ -1,9 +1,13 @@
 # PROBLEM 1:
 
-name = input("enter your name")
-print(f"good afternoon,{name}") # f keyword and {} use print string 
+# name = input("enter your name")
+# print(f"good afternoon,{name}") # f keyword and {} use print string 
 
 
 # PROBLEM 2:
 
-letter
+letter ='''Dear <|Name|>,
+You are selected!
+<|Date|> '''
+
+print(letter.replace("<|Name|>", "shivam").replace("<|Date|","24 september 2026"))

adf259e855aabff87cb46b6ab66c5fc5c073e6d9
5hivam45 <shivam2722007@gmail.com>
2026-09-13 23:16:59 +0530

    notes

--- chap3 precticeset.py
+++ chap3 precticeset.py
@@ -1,26 +1,29 @@
 # PROBLEM 1:
 
-name = input("enter your name")
-print(f"good afternoon,{name}") # f keyword and {} use print string 
+# name = input("enter your name")
+# print(f"good afternoon,{name}") # f keyword and {} use print string 
 
 
-# PROBLEM 2:
+# # PROBLEM 2:
 
-letter ='''Dear <|Name|>,
-You are selected!
-<|Date|> '''
+# letter ='''Dear <|Name|>,
+# You are selected!
+# <|Date|> '''
 
-print(letter.replace("<|Name|>", "shivam").replace("<|Date|","24 september 2026"))
+# print(letter.replace("<|Name|>", "shivam").replace("<|Date|","24 september 2026"))
 
-# problem 3 :
+# # problem 3 :
 
-name = "shivam is good  boy "
-print(name.find("  "))
+# name = "shivam is good  boy "
+# print(name.find("  "))
 
-# problem 4:
+# # problem 4:
 
-name = "shivam is a good  boy"
-print(name,replace("  "," ")) #string is imutable wich means that you cannot change them by running function on them
+# name = "shivam is a good  boy"
+# print(name,replace("  "," ")) #string is imutable wich means that you cannot change them by running function on them
 
+# problem 5:
 
+letter="dear harry,\nthis python course is nice.\nthanks!"
 
+print(letter)

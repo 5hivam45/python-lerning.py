47911c1ac7584502d58d4d590004f6fced846b21
5hivam45 <shivam2722007@gmail.com>
2026-09-14 23:18:10 +0530

    notes

--- chap4 list&tuples.py
+++ chap4 list&tuples.py
@@ -1,11 +1,18 @@
 # LIST:python lit are containers to store a set of value of any data type 
 
 # 1:
-name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
-print(name[2]) #OUTPUT=5
+# name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+# print(name[2]) #OUTPUT=5
 
-# 2:
+# # 2:
+# name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+# name[2]=8
+# print(name[2]) #unlike stings lists are mutable 
+# print(name) #OUTPUT=['apple', 'orange', 8, 345.06, False, 'Aakash', 'shivam']
+
+# 3:
 name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
-name[2]=8
-print(name[2])
-print(name) #OUTPUT=['apple', 'orange', 8, 345.06, False, 'Aakash', 'shivam']
+print(name[1:3]) #OUTPU=['ORANGE',5]
+
+
+# LIST METHODS:

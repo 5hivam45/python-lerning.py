cc7fe8c915d3d471ca5bfd3ab33189d428822c1c
5hivam45 <shivam2722007@gmail.com>
2026-09-14 22:45:28 +0530

    notes

---
+++ chap4 list&tuples.py
@@ -0,0 +1,11 @@
+# LIST:python lit are containers to store a set of value of any data type 
+
+# 1:
+name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+print(name[2]) #OUTPUT=5
+
+# 2:
+name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+name[2]=8
+print(name[2])
+print(name) #OUTPUT=['apple', 'orange', 8, 345.06, False, 'Aakash', 'shivam']

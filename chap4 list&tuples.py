10c9a4ad57854b60e0424b194d45e53a16bca6bf
5hivam45 <shivam2722007@gmail.com>
2026-09-15 20:17:03 +0530

    notes

--- chap4 list&tuples.py
+++ chap4 list&tuples.py
@@ -11,8 +11,11 @@
 # print(name) #OUTPUT=['apple', 'orange', 8, 345.06, False, 'Aakash', 'shivam']
 
 # 3:
-name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
-print(name[1:3]) #OUTPU=['ORANGE',5]
+# name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+# print(name[1:3]) #OUTPU=['ORANGE',5]
 
 
 # LIST METHODS:
+name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+name.append("ishan")
+print(name) #OUTPUT=['apple', 'orange', 5, 345.06, False, 'Aakash', 'shivam', 'ishan']
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+name = ["apple","orange",5,345.06,False,"Aakash","shivam"]
+# print(name[1:3]) #OUTPU=['ORANGE',5]

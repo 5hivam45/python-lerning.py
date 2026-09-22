997f5660833a30d08001aeac24b2ede877c42e45
5hivam45 <shivam2722007@gmail.com>
2026-09-22 16:19:59 +0530

    notes

---
+++ chap4 precticeset.py
@@ -0,0 +1,27 @@
+fruits = []
+
+f1 = input("Enter fruits name :")
+fruits.append(f1)                    
+f2 = input("Enter fruits name :")
+fruits.append(f2)
+f3 = input("Enter fruits name :")
+fruits.append(f3)
+f4 = input("Enter fruits name :")
+fruits.append(f4)
+f5 = input("Enter fruits name :")
+fruits.append(f5)
+
+print(fruits)  
+
+# OUTPUT WILL BE:
+# Enter fruits name :apple 
+# Enter fruits name :bannana 
+# Enter fruits name :watermelon
+# Enter fruits name :orange 
+# Enter fruits name :jamun
+# ['apple ', 'bannana', 'watermelon', 'orange', 'jamun']
+
+
+
+
+

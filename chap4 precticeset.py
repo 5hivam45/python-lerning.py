378e41131879500d5f05f68b59985d88f37e2884
5hivam45 <shivam2722007@gmail.com>
2026-09-22 16:27:35 +0530

    notes

--- chap4 precticeset.py
+++ chap4 precticeset.py
@@ -1,17 +1,19 @@
-fruits = []
+# PROBLEM = 1
 
-f1 = input("Enter fruits name :")
-fruits.append(f1)                    
-f2 = input("Enter fruits name :")
-fruits.append(f2)
-f3 = input("Enter fruits name :")
-fruits.append(f3)
-f4 = input("Enter fruits name :")
-fruits.append(f4)
-f5 = input("Enter fruits name :")
-fruits.append(f5)
+# fruits = []
 
-print(fruits)  
+# f1 = input("Enter fruits name :")
+# fruits.append(f1)                    
+# f2 = input("Enter fruits name :")
+# fruits.append(f2)
+# f3 = input("Enter fruits name :")
+# fruits.append(f3)
+# f4 = input("Enter fruits name :")
+# fruits.append(f4)
+# f5 = input("Enter fruits name :")
+# fruits.append(f5)
+
+# print(fruits)  
 
 # OUTPUT WILL BE:
 # Enter fruits name :apple 
@@ -21,7 +23,28 @@ print(fruits)
 # Enter fruits name :jamun
 # ['apple ', 'bannana', 'watermelon', 'orange', 'jamun']
 
+# PROBLEM = 2
 
+marks = []
 
+f1 = input("Enter marks name :")
+marks.append(f1)                    
+f2 = input("Enter marks name :")
+marks.append(f2)
+f3 = input("Enter marks name :")
+marks.append(f3)
+f4 = input("Enter marks name :")
+marks.append(f4)
+f5 = input("Enter marks name :")
+marks.append(f5)
 
+print(marks)  
+
+# OUTPUT WLL BE :
+# Enter marks name :25
+# Enter marks name :45
+# Enter marks name :63
+# Enter marks name :78
+# Enter marks name :96
+# ['25', '45', '63', '78', '96']
 

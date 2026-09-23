ed6324498f6fa750c5c8826bdf75166baa3d9f6f
5hivam45 <shivam2722007@gmail.com>
2026-09-23 19:25:57 +0530

    notes

--- chap5 dictionary & sets.py
+++ chap5 dictionary & sets.py
@@ -1,16 +1,37 @@
-marks = {
-    "shivam": 100,
-    "shivang": 80,
-    "ishan":50,
-    "somil":20 ,  #OUTPUT WILL BE ={'shivam': 100, 'shivang': 80, 'ishan': 50, 'somil': 20} <class 'dict'>
-}
-print(marks,type(marks))
-
-marks = {
-    "shivam": 100,
-    "shivang": 80,
-    "ishan":50,
-    "somil":20
+# DECTIONARY:dictionary is a collection of keys-value pairs.
+# SYNTAX:
+# a= {"key":"value",
+#      "harry":"code",
+#      "marks":"100",
+#      "list":[1,2,9]   
+# }
+# print(a["key"])   # OUTPUT WILL BE =value
+# print(a['list'])  # OUTPUT WILL BE =[1,2,9]
+     
     
-}
-print(marks["shivam"])   #output will be = 100
+# EXAMPLE:
+
+# marks = {
+#     "shivam": 100,
+#     "shivang": 80,
+#     "ishan":50,
+#     "somil":20 ,  #OUTPUT WILL BE ={'shivam': 100, 'shivang': 80, 'ishan': 50, 'somil': 20} <class 'dict'>
+# }
+# print(marks,type(marks))
+
+# EXAMPLE:
+
+# marks = {
+#     "shivam": 100,
+#     "shivang": 80,
+#     "ishan":50,
+#     "somil":20
+    
+# }
+# print(marks["shivam"])   #output will be = 100
+
+# PROPERTIES OF PYTHON DICTIONARIES
+# 1. it is unordered
+# 2. it is mutable
+# 3. it is indexed
+# 4. cannot contain duplicate keys

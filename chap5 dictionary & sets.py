fdf8628b50469ea8bdb46260dd17f7edf3bb1b99
5hivam45 <shivam2722007@gmail.com>
2026-09-23 18:54:52 +0530

    notes

---
+++ chap5 dictionary & sets.py
@@ -0,0 +1,8 @@
+marks = {
+    "shivam": 100,
+    "shivang": 80,
+    "ishan":50,
+    "somil":20 ,  #OUTPUT WILL BE ={'shivam': 100, 'shivang': 80, 'ishan': 50, 'somil': 20} <class 'dict'>
+}
+print(marks,type(marks))
+
